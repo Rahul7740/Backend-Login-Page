1aec1a021514cffbbc1257fa1da3781041084619
Rahul7740 <Rahultaak7740@gmail.com>
2024-10-23 19:35:47 +0530

    update code

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -20,6 +20,17 @@ Data:{
 }
 email-verify-enter-> otp
 
+res:{
+    "data": {
+        "id": 8,
+        "name": "Rahul123",
+        "email": "RahulTaak123@gmail.com",
+        "password": "$2b$10$d1IETPFtRnjUH/a7xm3yge8Kk3zIIm5fVwZs48Tz72jpZcEsSPp5i",
+        "updatedAt": "2024-10-23T14:04:38.388Z",
+        "createdAt": "2024-10-23T14:04:38.388Z"
+    },
+    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwiZW1haWwiOiJSYWh1bFRhYWsxMjNAZ21haWwuY29tIiwiaWF0IjoxNzI5NjkyMjc4LCJleHAiOjE3Mjk2OTk0Nzh9.JLo7I3GzS-7obikbb_MCbDGNVqKTQgJA7M-KM1pHyrQ"
+}
 
 ==========login:=======
 
